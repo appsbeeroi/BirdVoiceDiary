3ad6dc57c0773d829553029e3aa8f274c6d9d7fc
esplusnin <esplusnin94@yandex.ru>
2025-10-09 12:00:38 +0300

    Initial Commit

---
+++ BirdVoiceDiary/BirdVoiceDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdVoiceDiaryApp.swift
+//  BirdVoiceDiary
+//
+//  Created by Евгений on 09.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BirdVoiceDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
